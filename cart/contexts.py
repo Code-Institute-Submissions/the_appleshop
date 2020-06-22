414160a052c1c1733ca81db661f9bb0737e46072
Peer Naupold <greenion@gmx.de>
2020-06-22 07:17:54 +0200

    corrected  test on cart contexts

--- cart/contexts.py
+++ cart/contexts.py
@@ -12,9 +12,10 @@ def cart_contents(request):
     every page
     """
     cart = request.session.get('cart', {})
-    cart_items = []
     if request.user.is_authenticated:
         sync_carts(request)
+    #cart = request.session.get('cart', {})
+    cart_items = []
     total = 0
     product_count = 0
     for id, quantity in cart.items():
@@ -57,10 +58,16 @@ def make_cart_dict(productstring, quantitystring):
 
 def merge_carts(tmp_cart_from_db, cart):
     merged_cart = cart
+    quantity_change=False
     for id, quantity in tmp_cart_from_db.items():
         if id not in merged_cart:
             merged_cart[id]=quantity
-    return merged_cart
+        elif id in merged_cart:
+            if tmp_cart_from_db[id] > merged_cart[id]:
+                merged_cart[id]=quantity
+                quantity_change=True
+
+    return merged_cart, quantity_change
 
 
 @login_required
@@ -83,11 +90,13 @@ def sync_carts(request):
         else:
             tmp_cart_db=yaml.load(user_cart.product_list, Loader=yaml.FullLoader)
             merged_cart = merge_carts(tmp_cart_db, cart)
-            user_cart.product_list = str(merged_cart)
+            user_cart.product_list = str(merged_cart[0])
             user_cart.save()
             request.session['cart'] = merged_cart
+            if merged_cart[1]:
+                messages.success(request, "Order quantity has been updated for some products with higher value.")
 
-    elif user_cart.product_list=="": 
+    elif user_cart.product_list=="":
         if cart != {}:
             user_cart.product_list = str(cart)
             user_cart.save()
