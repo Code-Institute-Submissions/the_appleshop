2372b4d5b5f65fd82a4ed6748dbe8090eb7033dc
Peer Naupold <greenion@gmx.de>
2020-06-25 23:06:01 +0200

    added new S3 bucket

--- appleshop/settings.py
+++ appleshop/settings.py
@@ -168,22 +168,24 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=94608000'
 }
 
-AWS_STORAGE_BUCKET_NAME = 'poldis-ecommerce'
+#AWS_STORAGE_BUCKET_NAME = 'poldis-ecommerce'
+AWS_STORAGE_BUCKET_NAME = 'the-appleshop'
 AWS_S3_REGION_NAME = 'eu-west-1'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 STATICFILES_LOCATION = 'static'
-#Removed to use local staticfiles
-#STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
-#Added to use local staticfiles
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if os.getenv("USELOCALSTATIC"):
+    #Added to use local staticfiles
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+else:
+    #Removed to use local staticfiles
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
