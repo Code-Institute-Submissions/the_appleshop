20a861fddd5f7b3d1b9d2f5c8d9951417b85b830
Peer Naupold <greenion@gmx.de>
2020-06-27 08:54:26 +0200

    implemented search function

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from .views import do_search
+from .views import do_search, do_ext_search
 
 urlpatterns = [
     url(r'^$', do_search, name='search'),
+    url(r'^ext_search/$', do_ext_search, name='ext_search'),
+
 ]
--- search/views.py
+++ search/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from products.models import Product
 
 def do_search(request):
-    products = Product.objects.filter(name__icontains=request.GET['q'])
+    products = Product.objects.filter(name__icontains=request.GET.get('q'))
     return render(request, 'products.html', {'products': products})
 
-# def do_ext_search(request):
-#     if request.method=="POST":
+def do_ext_search(request):
+    searchterm=request.GET.get('q', "")
+    products = Product.objects.filter(name__icontains=searchterm)
+    return render(request, 'search.html', {'products': products, 'searchterm': 'searchterm'})
+
+
+
+
 
